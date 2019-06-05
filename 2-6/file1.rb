614075dc6d95801c29a0be72765a7e06592dcf7d
西雄太 <nishiyuuta@nishiyuutanoMacBook-Pro.local>
2019-06-05 13:12:11 +0900

    [Finish]2-6

---
+++ 2-6/file1.rb
@@ -0,0 +1,16 @@
+puts 100
+
+puts 100+5
+
+rb="ruby"
+we="web"
+cp="camp"
+puts "webcamp ruby"
+puts "webcamp "+rb
+puts we+cp+rb
+
+names=["web","camp","pro"]
+puts names[0]+names[1]+names[2]
+
+tall={1=>"web",2=>"camp",3=>"pro"}
+puts tall[1]+tall[2]+tall[3]
