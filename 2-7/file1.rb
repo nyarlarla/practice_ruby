845d27cad2e02bb95037c1ba31bd4dac84e4a6a5
西雄太 <nishiyuuta@nishiyuutanoMacBook-Pro.local>
2019-06-05 13:42:50 +0900

    [Finish]2-7

---
+++ 2-7/file1.rb
@@ -0,0 +1,32 @@
+apple=100
+
+if apple>=100
+	puts"リンゴの値段は100円以上です"
+end
+
+if apple<=100
+	puts"リンゴの値段は100円以下です"
+end
+
+tall=180
+
+if tall>=170 && tall<=190
+	puts"身長は170以上190以下です"
+end
+
+apple="Aomori"
+
+if apple=="Nagano"
+	puts "リンゴの生産地は長野です"
+else
+	puts "リンゴの生産地は青森です"
+end
+
+if apple=="yamagata"
+	puts "リンゴの生産地は長野です"
+elsif apple=="yamagata"
+	puts"リンゴの生産地は山形です"
+else
+	puts "リンゴの生産地は青森です"
+end
+
